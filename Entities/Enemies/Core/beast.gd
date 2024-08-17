645c5ebf02f7da6003a8f3bf23f04db4c50b859f
cag39 (pufu) <cagrisirtas2@hotmail.com>
2024-08-17 15:41:15 +0300

    Hurtbox generation and general fixes

--- Entities/Enemies/Core/beast.gd
+++ Entities/Enemies/Core/beast.gd
@@ -7,22 +7,32 @@ class_name Beast extends CharacterBody2D
 #-A maximum/current health stat that can be decreased by attacking gooblins
 #-A variable size “attack area” that corresponds to its attack or kick animations
 
-@onready var state_machine = $StateMachine
-
-@export var health := 100 #placeholder value
-
 var gravity : int = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var state_machine: Node = $StateMachine
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+@export var max_health := 100 #placeholder
+var health = max_health
+
+# attackareas dictionary?
+
 
 # Actual logic
 func _ready():
-	state_machine.state_changed.connect(fsm_state_changed)
+	state_machine.state_changed.connect(_fsm_state_changed)
 
 func _process(_delta):
 	pass
 
 func _physics_process(delta: float) -> void:
+	#should probably move CHaracterBody2D functionality to the parent
 	velocity.y += gravity * delta
 	move_and_slide()
 
-func fsm_state_changed(state):
-	prints("the enemy is registering that the state changed to", state)
+func _fsm_state_changed(state):
+	#prints(owner.name,"is registering that the state changed to", state)
+	animation_player.play(state)
+
+func take_damage(dmg):
+	health -= dmg
