b19b5737223a6ee2f3c14bf9892018c88aa69a5c
thecaptaincarrot <trevorlandersen@gmail.com>
2024-08-18 15:17:44 -0500

    Upgrade Shop complete
    All nodes should be referenced via export variable, so we can redesign the UI as needed

--- Menus/OutOfCombatr/army_comp_panel.gd
+++ Menus/OutOfCombatr/army_comp_panel.gd
@@ -1,11 +1,41 @@
 extends Panel
 
 
+#label references
+@export var available_gooblins_label : Label
+
+@export var available_shields_label : Label
+
+#Spinbox References
+@export var shield_gooblin_spinbox : SpinBox
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	update_all() # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+#updates
+func update_available_gooblins(): #THIS MUST ALWAYS BE DONE FIRST AND I'M SORRY ABOUT THAT
+	GooblinUpgrades.basic_gooblins = GooblinUpgrades.gooblin_count - GooblinUpgrades.shield_gooblins - GooblinUpgrades.climb_gooblins
+	available_gooblins_label.text = str(GooblinUpgrades.basic_gooblins)
+
+
+func update_shield_gooblins_controls(): #THIS DOES NOT CHANGE THE ACTUAL NUMBER OF SHIELD GOOBLINS
+	shield_gooblin_spinbox.max_value = GooblinUpgrades.shield_gooblins + min(GooblinUpgrades.basic_gooblins,GooblinUpgrades.shields - GooblinUpgrades.shield_gooblins)
+	available_shields_label.text = str(GooblinUpgrades.shields - GooblinUpgrades.shield_gooblins)
+
+
+func update_all():
+	update_available_gooblins()
+	update_shield_gooblins_controls()
+
+#signal responses
+func _on_shield_spinbox_value_changed(value):
+	GooblinUpgrades.shield_gooblins = value
+	update_all()
