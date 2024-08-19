436d6f889e787c8f14f8faa220fb20d640fcfd8a
thecaptaincarrot <trevorlandersen@gmail.com>
2024-08-19 17:05:52 -0500

    Non-Combat Screen finished
    Map textures pulled too

--- Screens/OutOfCombat/army_comp_panel.gd
+++ Screens/OutOfCombat/army_comp_panel.gd
@@ -6,9 +6,12 @@ extends Panel
 
 @export var available_shields_label : Label
 
+@export var available_hook_label : Label
+
 #Spinbox References
 @export var shield_gooblin_spinbox : SpinBox
 
+@export var hook_gooblin_spinbox : SpinBox
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -31,11 +34,22 @@ func update_shield_gooblins_controls(): #THIS DOES NOT CHANGE THE ACTUAL NUMBER 
 	available_shields_label.text = str(GooblinUpgrades.shields - GooblinUpgrades.shield_gooblins)
 
 
+func update_hook_gooblins_controls():
+	hook_gooblin_spinbox.max_value = GooblinUpgrades.climb_gooblins + min(GooblinUpgrades.basic_gooblins,GooblinUpgrades.hooks - GooblinUpgrades.climb_gooblins)
+	available_hook_label.text = str(GooblinUpgrades.hooks - GooblinUpgrades.climb_gooblins)
+
+
 func update_all():
 	update_available_gooblins()
 	update_shield_gooblins_controls()
+	update_hook_gooblins_controls()
 
 #signal responses
 func _on_shield_spinbox_value_changed(value):
 	GooblinUpgrades.shield_gooblins = value
+	update_all()
+
+
+func _on_scalers_spinbox_value_changed(value):
+	GooblinUpgrades.climb_gooblins = value
 	update_all()
--- Screens/OutOfCombat/out_of_combat.gd
+++ Screens/OutOfCombat/out_of_combat.gd
@@ -2,6 +2,18 @@ extends Control
 
 signal go_to_combat
 
+@export var background_texture : TextureRect
+
+@export var background_scroll_divider = 10
+
+@export var shop_control : Panel
+
+@export var planning_menus : Control
+@export var army_comp_panel : Panel
+@export var army_display : Node2D
+
+var direction = -1
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,9 +25,33 @@ func _process(delta):
 
 
 func _physics_process(delta):
-	Texture.
+	#if background_texture.texture is GradientTexture1D:
+		#var offset_0 = background_texture.texture.gradient.get_offset(0)
+		#background_texture.texture.gradient.set_offset(0,offset_0 - delta / background_scroll_divider)
+		#
+		#var offset_1 = background_texture.texture.gradient.get_offset(1)
+		#
+		#background_texture.texture.gradient.set_offset(1,offset_1 + direction * ( delta / background_scroll_divider)
+	pass
+
+
+func reset_to_default():
+	$ShopPanel.hide()
+	
+
 
 
 func _on_to_combat_button_pressed() -> void:
 	var level = 0
 	emit_signal("go_to_combat",level)
+
+
+func _on_shop_button_pressed():
+	planning_menus.hide()
+	shop_control.update_all()
+	shop_control.show()
+
+
+func _on_return_button_pressed():
+	planning_menus.show()
+	shop_control.hide()
