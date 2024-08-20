8b23037b8dc3bb78ca9bdc8455075c36d8a80baf
thecaptaincarrot <trevorlandersen@gmail.com>
2024-08-19 23:08:00 -0500

    Unique blood colors
    (and restored a missing file)

---
+++ Entities/Enemies/Wyrm/wyrm.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var enemy = $Beast
+@onready var animation_player: AnimationPlayer = $Beast/AnimationPlayer
+@export var horde_controller: GooblinHordeController
+var target_list: Array
+
+func _ready() -> void:
+	enemy.reacquire_targets.connect(reacquire_targets)
+	reacquire_targets()
+	enemy.died.connect(die)
+
+func reacquire_targets():
+	target_list = horde_controller.get_basic_gooblins()
+
+func die():
+	enemy.state_machine.change_to_state("DeadState")
+
+# Surface level dmg function for the horde to access
+func take_damage(dmg):
+	enemy.take_damage(dmg)
