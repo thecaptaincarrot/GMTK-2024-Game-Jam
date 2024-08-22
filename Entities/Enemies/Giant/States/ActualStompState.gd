496533ef588a03511548563567733be3a47fab3d
cag39 (pufu) <cagrisirtas2@hotmail.com>
2024-08-23 00:26:17 +0300

    Redo giant animations

--- Entities/Enemies/Giant/States/ActualStompState.gd
+++ Entities/Enemies/Giant/States/ActualStompState.gd
@@ -8,23 +8,15 @@ extends GenericState
 func enter(_msg):
 	beast.random_target_timer.stop()
 	stomper.disabled = false
-	var prev = leg_target.position
-	var tween
-	tween = get_tree().create_tween()
-	tween.tween_property(leg_target, "position", leg_peak, peak_time).set_trans(Tween.TRANS_SPRING)
-	await tween.finished
-	
-	tween = get_tree().create_tween()
-	tween.tween_property(leg_target, "position", prev, stomp_time).set_trans(Tween.TRANS_QUINT)
-	await tween.finished
-	
-	hurt_gooblins()
-	emit_signal("screen_shake")
-	flingerize_gooblins()
+
+	beast.animation_tree["parameters/LegAction/conditions/stomping"] = true
+	beast.animation_tree["parameters/Leg/request"] = AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE
+	await beast.animation_tree.animation_finished
 	
 	state_machine.change_to_state("IdleState")
 
 func exit():
 	# once it's done
 	stomper.disabled = true
+	beast.animation_tree["parameters/LegAction/conditions/stomping"] = false
 	beast.random_target_timer.start()
